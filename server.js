2dc238ce87b958139a88a0eef947c46d8de82c54
shubham kumar <shubhofficial1@gmail.com>
2023-03-15 22:39:42 +0530

    Socket IO Initial Setup

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const http = require("http");
+const expressServer = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(expressServer);
+
+io.on("connection", (socket) => {
+  console.log("New User Connected !");
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected !");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+const PORT = process.env.PORT || 8080;
+const MODE = process.env.MODE || "development";
+
+expressServer.listen(PORT, () => {
+  console.log(`Server is running in ${MODE} on PORT ${PORT}`);
+});
