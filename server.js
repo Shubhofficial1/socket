73096af053342d7ac56aeb381c5b500938122f5f
shubham kumar <shubhofficial1@gmail.com>
2023-03-16 01:21:35 +0530

    Implemented Namespace

--- server.js
+++ server.js
@@ -6,50 +6,57 @@ const expressServer = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(expressServer);
 
-io.on("connection", (socket) => {
-  console.log("New User Connected !");
+// io.on("connection", (socket) => {
+// console.log("New User Connected !");
 
-  //   socket.on("disconnect", () => {
-  //     console.log("User Disconnected !");
-  //   });
+//   socket.on("disconnect", () => {
+//     console.log("User Disconnected !");
+//   });
 
-  // Implementation for Data transfer after some time
+// Implementation for Data transfer after some time
 
-  //   setTimeout(() => {
-  //     socket.send("Hello There");
-  //   }, 5000);
+//   setTimeout(() => {
+//     socket.send("Hello There");
+//   }, 5000);
 
-  // Implementation of Data transfer on certain intervals
+// Implementation of Data transfer on certain intervals
 
-  //   setInterval(() => {
-  //     let date = new Date();
-  //     let time = date.getTime();
-  //     socket.send(time);
-  //   }, 1000);
+//   setInterval(() => {
+//     let date = new Date();
+//     let time = date.getTime();
+//     socket.send(time);
+//   }, 1000);
 
-  // Implementation of custom event
+// Implementation of custom event
 
-  //   setInterval(() => {
-  //     let date = new Date();
-  //     let time = date.getTime();
-  //     socket.emit("MyEvent", time);
-  //   }, 1000);
+//   setInterval(() => {
+//     let date = new Date();
+//     let time = date.getTime();
+//     socket.emit("MyEvent", time);
+//   }, 1000);
 
-  //   Getting Data from client to server
+//   Getting Data from client to server
 
-  //   socket.on("message", (msg) => {
-  //     console.log(msg);
-  //   });
+//   socket.on("message", (msg) => {
+//     console.log(msg);
+//   });
 
-  //   getting Data from client to server using custom Event
+//   getting Data from client to server using custom Event
 
-  // socket.on("MyEvent", (msg) => {
-  //   console.log(msg);
-  // });
+// socket.on("MyEvent", (msg) => {
+//   console.log(msg);
+// });
 
-  // Broadcast Implementation
+// Broadcast Implementation
 
-  io.sockets.emit("MyBroadcast", "Hello World");
+// io.sockets.emit("MyBroadcast", "Hello World");
+// });
+
+// Namespace
+
+let first = io.of("/first");
+first.on("connection", (socket) => {
+  first.emit("MyEvent", "Hello First");
 });
 
 app.get("/", (req, res) => {
